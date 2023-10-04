e07fcfe3d22c5b1262f13669d2b62ddd6aab0a40
4sh <80625238+Recentaly@users.noreply.github.com>
2023-10-04 18:19:11 +0200

    Update chat.py

--- modules/chat.py
+++ modules/chat.py
@@ -2,30 +2,10 @@
 from vercel_ai import Client
 from curl_cffi.requests.errors import RequestsError
 
-def chat(client: Client, messages: list, model: str = "openai:gpt-3.5-turbo", params: dict = {"temperature": 0.8}) -> None:
-
-    response: str = ""
-
-    try:
-
-        for chunk in client.chat(model, messages, params):
-
-            # just make sure we dont process the returned error lol
-            if chunk != 'Internal Server Error':
-
-                response += chunk
-                print(chunk, end="", flush=True)
-
-        # append the ai's response to the message list
-        messages.append({'role': 'assistant', 'content': f'{response}'})
-
-    # error-driven recursive call
-    except RequestsError:
-
-        chat(client, messages, params=params, model=model)
+from json import dumps
 
 # generative, doesn't print
-def chat_gen(client: Client, messages: list, model: str = "openai:gpt-3.5-turbo", params: dict = {"temperature": 0.8}) -> str:
+def chat_gen(client: Client, messages: list, model: str = "openai:gpt-3.5-turbo", params: dict = {"temperature": 0.8, "maxTokens": 500}) -> str:
 
     response: str = ""
 
@@ -35,8 +15,18 @@ def chat_gen(client: Client, messages: list, model: str = "openai:gpt-3.5-turbo"
 
             # just make sure we dont process the returned error lol
             if chunk != 'Internal Server Error':
+            
+                if "help.openai.com" not in chunk:
 
-                response += chunk
+                    response += chunk
+
+                else:
+
+                    return chat_gen(Client(client.proxy), messages, model, params)
+
+                
+                # check if we're rate limited :sad_face_
+                #print(f"\n{chunk}\n")
 
         # append the ai's response to the message list
         messages.append({'role': 'assistant', 'content': f'{response}'})

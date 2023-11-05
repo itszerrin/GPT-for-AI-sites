148a9f4147e66d01911c13357a8aac0924822863
4sh <80625238+Recentaly@users.noreply.github.com>
2023-11-05 18:15:22 +0100

    Delete modules/client.py

--- modules/client.py
+++
@@ -1,8 +0,0 @@
-from vercel_ai import Client
-
-# function to create a new client for generation
-def newClient(proxy: str = None) -> Client:
-
-    #return Client(proxy=proxy)
-    return Client(proxy)
-

e6dac68c29404a1fb4594e02d4bffb3687a0da51
4sh <80625238+Recentaly@users.noreply.github.com>
2023-11-09 06:29:58 +0100

    Update chat.py

--- modules/chat.py
+++ modules/chat.py
@@ -12,7 +12,7 @@ def chat_gen(model: str, messages: list) -> str:
         response = ChatCompletion.create(
             model=model,
             messages=messages,
-            provider=Provider.Yqcloud,
+            provider=Provider.ChatgptX,
             stream=False, # streaming support coming soon
         )
 

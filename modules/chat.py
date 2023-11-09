fa1428c228d83a2aa90ea978be62d0cfe69f3a07
4sh <80625238+Recentaly@users.noreply.github.com>
2023-11-09 05:38:16 +0100

    Update chat.py

--- modules/chat.py
+++ modules/chat.py
@@ -1,5 +1,5 @@
 # only necessary imports
-from g4f import ChatCompletion
+from g4f import ChatCompletion, Provider
 
 # generative, doesn't print
 def chat_gen(model: str, messages: list) -> str:
@@ -12,6 +12,7 @@ def chat_gen(model: str, messages: list) -> str:
         response = ChatCompletion.create(
             model=model,
             messages=messages,
+            provider=Provider.Liaobots,
             stream=False, # streaming support coming soon
         )
 

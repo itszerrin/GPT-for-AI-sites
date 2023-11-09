a0558fbc817094bbbde2ab96c1a6718a9837e68e
4sh <80625238+Recentaly@users.noreply.github.com>
2023-11-09 06:40:47 +0100

    Update chat.py

--- modules/chat.py
+++ modules/chat.py
@@ -12,7 +12,7 @@ def chat_gen(model: str, messages: list) -> str:
         response = ChatCompletion.create(
             model=model,
             messages=messages,
-            provider=Provider.Llama2,
+            provider=Provider.AiAsk,
             stream=False, # streaming support coming soon
         )
 

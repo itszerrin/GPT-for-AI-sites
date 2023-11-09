55e43edce1d6c7fd3124fe78f6c9ff2e6b1349ae
4sh <80625238+Recentaly@users.noreply.github.com>
2023-11-09 05:56:40 +0100

    Update chat.py

--- modules/chat.py
+++ modules/chat.py
@@ -12,8 +12,6 @@ def chat_gen(model: str, messages: list) -> str:
         response = ChatCompletion.create(
             model=model,
             messages=messages,
-            provider=Provider.Bing,
-            cookies={"Fake": ""},
             stream=False, # streaming support coming soon
         )
 

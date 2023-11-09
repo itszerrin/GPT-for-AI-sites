f6458011852bb9d251c1c723d19b6787dc7df91f
4sh <80625238+Recentaly@users.noreply.github.com>
2023-11-09 05:45:37 +0100

    Update chat.py

--- modules/chat.py
+++ modules/chat.py
@@ -12,7 +12,7 @@ def chat_gen(model: str, messages: list) -> str:
         response = ChatCompletion.create(
             model=model,
             messages=messages,
-            provider=Provider.Phind,
+            provider=Provider.Bing,
             stream=False, # streaming support coming soon
         )
 

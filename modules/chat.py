f170bc3e22442911820976d8218cef8054f4c6e5
4sh <80625238+Recentaly@users.noreply.github.com>
2023-11-20 23:17:12 +0100

    Update chat.py

--- modules/chat.py
+++ modules/chat.py
@@ -15,7 +15,7 @@ def chat_gen(model: str, messages: list) -> str:
         response = ChatCompletion.create(
             model=model,
             messages=messages,
-            provider=Provider.Chatgpt4Online,
+            provider=Provider.GptGod,
             stream=False, # streaming support coming soon
         )
 

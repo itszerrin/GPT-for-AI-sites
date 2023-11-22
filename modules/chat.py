15115abac91acc7a032d40f99828d3a9b91ab64d
4sh <80625238+Recentaly@users.noreply.github.com>
2023-11-22 06:53:37 +0100

    Update chat.py

--- modules/chat.py
+++ modules/chat.py
@@ -15,7 +15,7 @@ def chat_gen(model: str, messages: list) -> str:
         response = ChatCompletion.create(
             model=model,
             messages=messages,
-            provider=Provider.ChatAnywhere,
+            provider=Provider.GeekGpt,
             stream=False, # streaming support coming soon
         )
 
